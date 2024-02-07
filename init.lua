3cafb411007c7af3feb10557b4a34bed4b72cbf4
pgosar <pgosarsteam@gmail.com>
2024-02-07 13:12:11 -0600

    up

--- init.lua
+++ init.lua
@@ -15,15 +15,15 @@ local liblldb_path = MASON_PATH .. "codelldb/extension/lldb/lib/liblldb.so"
 local rt = require("rust-tools")
 
 rt.setup({
-	server = {
-		on_attach = function(_, bufnr)
-			-- Hover actions
-			vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-			-- Code action groups
-			vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-		end,
-	},
-	dap = {
-		adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
-	},
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+  dap = {
+    adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+  },
 })
--- user_config.lua
+++ user_config.lua
@@ -4,102 +4,104 @@ local M = {}
 
 -- add any null-ls sources you want here
 M.setup_sources = function(b)
-	return {
-		b.formatting.clang_format,
-		b.completion.luasnip,
-		b.completion.tags,
-		b.formatting.stylua,
-		b.formatting.autopep8,
-		b.formatting.beautysh,
-		b.formatting.cbfmt,
-		b.formatting.gofumpt,
-		b.formatting.jq,
-		b.formatting.cmake_format,
-		b.formatting.prettierd.with({
-			filetypes = {
-				"javascript",
-				"javascriptreact",
-				"typescript",
-				"typescriptreact",
-				"html",
-				"css",
-			},
-		}),
-		b.diagnostics.checkmake,
-		b.diagnostics.clang_check,
-		b.diagnostics.cmake_lint,
-		-- b.diagnostics.pylint,
-		b.diagnostics.revive,
-		-- b.diagnostics.xo,
-		b.code_actions.cspell,
-		-- b.code_actions.xo,
-		b.code_actions.gitrebase,
-		b.code_actions.gitsigns,
-		b.code_actions.gomodifytags,
-		b.code_actions.proselint,
-		b.code_actions.refactoring,
-		b.hover.dictionary,
-	}
+  return {
+    b.formatting.clang_format,
+    b.completion.luasnip,
+    b.completion.tags,
+    b.formatting.stylua,
+    b.formatting.autopep8,
+    b.formatting.beautysh,
+    b.formatting.cbfmt,
+    b.formatting.gofumpt,
+    b.formatting.jq,
+    b.formatting.cmake_format,
+    b.formatting.prettierd.with({
+      filetypes = {
+        "javascript",
+        "javascriptreact",
+        "typescript",
+        "typescriptreact",
+        "html",
+        "css",
+      },
+    }),
+    b.formatting.verible_verilog_format,
+    b.diagnostics.verilator,
+    b.diagnostics.checkmake,
+    b.diagnostics.clang_check,
+    b.diagnostics.cmake_lint,
+    -- b.diagnostics.pylint,
+    b.diagnostics.revive,
+    -- b.diagnostics.xo,
+    b.code_actions.cspell,
+    -- b.code_actions.xo,
+    b.code_actions.gitrebase,
+    b.code_actions.gitsigns,
+    b.code_actions.gomodifytags,
+    b.code_actions.proselint,
+    b.code_actions.refactoring,
+    b.hover.dictionary,
+  }
 end
 
 -- add null_ls sources to auto-install
 M.ensure_installed = {}
 
 M.formatting_servers = {
-	["rust_analyzer"] = { "rust" },
-	["null-ls"] = {
-		"lua",
-		"javascript",
-		"javascriptreact",
-		"typescript",
-		"typescriptreact",
-		"html",
-		"css",
-		"json",
-		"python",
-		"sh",
-		"bash",
-		"zsh",
-		"go",
-		"cpp",
-		"c",
-		"cmake",
-		"make",
-	},
+  ["rust_analyzer"] = { "rust" },
+  ["null-ls"] = {
+    "lua",
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+    "html",
+    "css",
+    "json",
+    "python",
+    "sh",
+    "bash",
+    "zsh",
+    "go",
+    "cpp",
+    "c",
+    "cmake",
+    "make",
+  },
 }
 
 M.enable_plugins = {
-	autosave = false,
+  autosave = false,
 }
 
 M.options = {
-	opt = {
-		swapfile = false,
-	},
+  opt = {
+    -- swapfile = false,
+  }
 }
 
 M.plugins = {
-	"simrat39/rust-tools.nvim",
-	"mfussenegger/nvim-dap-python",
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		event = "InsertEnter",
-		dependencies = {
-			"zbirenbaum/copilot-cmp",
-			config = function()
-				require("copilot_cmp").setup()
-			end,
-		},
-		config = function()
-			require("user.plugin-configs.copilot")
-		end,
-	},
+  "simrat39/rust-tools.nvim",
+  "mfussenegger/nvim-dap-python",
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    dependencies = {
+      "zbirenbaum/copilot-cmp",
+      config = function()
+        require("copilot_cmp").setup()
+      end,
+    },
+    config = function()
+      require("user.plugin-configs.copilot")
+    end,
+  },
 }
 
 M.user_conf = function()
-	require("user.init")
-	vim.g.rust_recommended_style = false
+  require("user.init")
+  vim.g.rust_recommended_style = false
 end
 
 return M
